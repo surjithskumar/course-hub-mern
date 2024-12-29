4c4ca03e8469117921046e9bd6df9ffe0eef7172
SURJITH S KUMAR <surjithskumar23@gmail.com>
2024-12-29 13:18:52 +0530

    basic setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <>
+      <Button className="bg-blue-300" >Check up</Button>
+    </>
+  )
+}
+
+export default App
