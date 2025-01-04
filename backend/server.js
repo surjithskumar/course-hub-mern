7a47f2e2577f4700e9659e497d08abf8caec8144
SURJITH S KUMAR <surjithskumar23@gmail.com>
2025-01-04 11:59:20 +0530

    auth and axios

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth-routes/index");
+const mediaRoutes = require("./routes/instructor-routes/media-routes");
+const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
+const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
+const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
+const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,9 +31,15 @@ mongoose
   .catch((e) => console.log(e));
 
 //routes configuration
-  
+app.use("/auth", authRoutes);
+app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
+app.use("/student/course", studentViewCourseRoutes);
+app.use("/student/order", studentViewOrderRoutes);
+app.use("/student/courses-bought", studentCoursesRoutes);
+app.use("/student/course-progress", studentCourseProgressRoutes);
 
-//global error handler
+// global error handler
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(500).json({
@@ -35,6 +48,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
